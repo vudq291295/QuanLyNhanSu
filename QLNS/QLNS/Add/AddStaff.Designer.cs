05b8dc765ab86827547b9d46f797a5f801744210
duy0308 <duy0308@gmail.com>
2017-05-27 01:35:13 +0700

    add nhan vien

--- QLNS/QLNS/Add/AddStaff.Designer.cs
+++ QLNS/QLNS/Add/AddStaff.Designer.cs
@@ -28,20 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpTTCoBan = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblMaNV = new System.Windows.Forms.Label();
+            this.lblTenNV = new System.Windows.Forms.Label();
+            this.lblNgaySinh = new System.Windows.Forms.Label();
+            this.lblGioiTinh = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.txtTenNV = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dtimeNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.cbbGioiTinh = new System.Windows.Forms.ComboBox();
+            this.grpTTCoBan.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grpTTCoBan
+            // 
+            this.grpTTCoBan.Controls.Add(this.cbbGioiTinh);
+            this.grpTTCoBan.Controls.Add(this.dtimeNgaySinh);
+            this.grpTTCoBan.Controls.Add(this.textBox4);
+            this.grpTTCoBan.Controls.Add(this.txtTenNV);
+            this.grpTTCoBan.Controls.Add(this.txtMaNV);
+            this.grpTTCoBan.Controls.Add(this.label5);
+            this.grpTTCoBan.Controls.Add(this.lblGioiTinh);
+            this.grpTTCoBan.Controls.Add(this.lblNgaySinh);
+            this.grpTTCoBan.Controls.Add(this.lblTenNV);
+            this.grpTTCoBan.Controls.Add(this.lblMaNV);
+            this.grpTTCoBan.Location = new System.Drawing.Point(12, 12);
+            this.grpTTCoBan.Name = "grpTTCoBan";
+            this.grpTTCoBan.Size = new System.Drawing.Size(645, 168);
+            this.grpTTCoBan.TabIndex = 0;
+            this.grpTTCoBan.TabStop = false;
+            this.grpTTCoBan.Text = "Thông tin cơ bản";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 200);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(645, 209);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin cá nhân";
+            // 
+            // lblMaNV
+            // 
+            this.lblMaNV.AutoSize = true;
+            this.lblMaNV.Location = new System.Drawing.Point(10, 36);
+            this.lblMaNV.Name = "lblMaNV";
+            this.lblMaNV.Size = new System.Drawing.Size(40, 13);
+            this.lblMaNV.TabIndex = 0;
+            this.lblMaNV.Text = "Mã NV";
+            // 
+            // lblTenNV
+            // 
+            this.lblTenNV.AutoSize = true;
+            this.lblTenNV.Location = new System.Drawing.Point(6, 68);
+            this.lblTenNV.Name = "lblTenNV";
+            this.lblTenNV.Size = new System.Drawing.Size(44, 13);
+            this.lblTenNV.TabIndex = 1;
+            this.lblTenNV.Text = "Tên NV";
+            // 
+            // lblNgaySinh
+            // 
+            this.lblNgaySinh.AutoSize = true;
+            this.lblNgaySinh.Location = new System.Drawing.Point(6, 95);
+            this.lblNgaySinh.Name = "lblNgaySinh";
+            this.lblNgaySinh.Size = new System.Drawing.Size(56, 13);
+            this.lblNgaySinh.TabIndex = 2;
+            this.lblNgaySinh.Text = "Ngày Sinh";
+            // 
+            // lblGioiTinh
+            // 
+            this.lblGioiTinh.AutoSize = true;
+            this.lblGioiTinh.Location = new System.Drawing.Point(15, 133);
+            this.lblGioiTinh.Name = "lblGioiTinh";
+            this.lblGioiTinh.Size = new System.Drawing.Size(47, 13);
+            this.lblGioiTinh.TabIndex = 3;
+            this.lblGioiTinh.Text = "Giới tính";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(423, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(56, 33);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(100, 20);
+            this.txtMaNV.TabIndex = 5;
+            // 
+            // txtTenNV
+            // 
+            this.txtTenNV.Location = new System.Drawing.Point(56, 65);
+            this.txtTenNV.Name = "txtTenNV";
+            this.txtTenNV.Size = new System.Drawing.Size(100, 20);
+            this.txtTenNV.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(386, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // dtimeNgaySinh
+            // 
+            this.dtimeNgaySinh.Location = new System.Drawing.Point(68, 95);
+            this.dtimeNgaySinh.Name = "dtimeNgaySinh";
+            this.dtimeNgaySinh.Size = new System.Drawing.Size(200, 20);
+            this.dtimeNgaySinh.TabIndex = 9;
+            // 
+            // cbbGioiTinh
+            // 
+            this.cbbGioiTinh.FormattingEnabled = true;
+            this.cbbGioiTinh.Location = new System.Drawing.Point(68, 130);
+            this.cbbGioiTinh.Name = "cbbGioiTinh";
+            this.cbbGioiTinh.Size = new System.Drawing.Size(121, 21);
+            this.cbbGioiTinh.TabIndex = 10;
             // 
             // AddStaff
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 299);
+            this.ClientSize = new System.Drawing.Size(669, 431);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grpTTCoBan);
             this.Name = "AddStaff";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Thêm mới nhân viên";
+            this.grpTTCoBan.ResumeLayout(false);
+            this.grpTTCoBan.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpTTCoBan;
+        private System.Windows.Forms.ComboBox cbbGioiTinh;
+        private System.Windows.Forms.DateTimePicker dtimeNgaySinh;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtTenNV;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblGioiTinh;
+        private System.Windows.Forms.Label lblNgaySinh;
+        private System.Windows.Forms.Label lblTenNV;
+        private System.Windows.Forms.Label lblMaNV;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
